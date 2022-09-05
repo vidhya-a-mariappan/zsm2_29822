7878b16ac614e55483fed77e50d97b99a7073af4
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-09-05 21:11:34 +0530

    Update 18_diasum.java

--- 18_diasum.java
+++ 18_diasum.java
@@ -2,24 +2,23 @@ import java.util.*;
 public class diasum
 {
 	public static void main(String[] args){
-		System.out.println("Enter the no of rows and columns");
+		System.out.println("Enter the no of rows whereas row = column");
 	    Scanner s=new Scanner(System.in);
-	    int row=s.nextInt();
-	    int col=s.nextInt();
-	    int[][] mat=new int[row][col];
+	    int n=s.nextInt();
+	    int[][] mat=new int[n][n];
 	    int dsum=0;
-	    System.out.println("Enter "+ (row+col)+" no of elements"); 
-	    for(int i=0;i<row;i++)
+	    System.out.println("Enter "+ (2*n)+" no of elements"); 
+	    for(int i=0;i<n;i++)
 	    {
-	        for(int j=0;j<col;j++)
+	        for(int j=0;j<n;j++)
 	        {
 	            mat[i][j]=s.nextInt();
 	            if(i==j)
-	                dsum+=mat[i][j];
+	               dsum+=mat[i][j];
 	        }
 	    }
 	    
-	    System.out.println(dsum);
+	    System.out.println("Sum of elements in diagonal " +dsum);
 	 }
 }
 
